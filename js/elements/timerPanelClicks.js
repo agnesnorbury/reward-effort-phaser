6221d4f710148eb51ac67ad6ceaa769dd8e5dc74
Agnes Norbury <agnes.norbury@cantab.net>
2022-01-26 10:43:15 +0000

    undid changes to timer panel text col and weight

--- js/elements/timerPanelClicks.js
+++ js/elements/timerPanelClicks.js
@@ -205,12 +205,13 @@ var createProgressBar = function(scene, trialEffort, backgrCol){
         
         text: scene.rexUI.add.BBCodeText(0, 0, '', {
             fontSize: '20px', fixedWidth: 50, fixedHeight: 45,
-            valign: 'center', halign: 'center', color: '#e45404'
+            valign: 'center', halign: 'center'//, color: '#e45404'
         }),
         
         valuechangeCallback: function (newValue, oldValue, progressBar, trialEffort) {
-            progressBar.text = '[b]'+(Math.round(newValue*100))+'%[/b]';
-        }
+            //progressBar.text = '[b]'+(Math.round(newValue*100))+'%[/b]';
+            progressBar.text = (Math.round(newValue*100))+'%';
+        } 
     })
     .setValue(0, 0, 1)  // initialize value at 0, on scale from 0 to 1
     .layout();
